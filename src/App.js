9d715fbc975e6e1a13cf3e8d3ce352be4646f4ee
uri1709 <uri1709k@gmail.com>
2024-09-07 01:17:23 +0600

    css

--- src/App.js
+++ src/App.js
@@ -1,62 +1,121 @@
 import logo from './logo.svg';
+import styles from './App.module.css';
 import './App.css';
-import { createElement } from 'react';
 
 export const App = () => {
-	const currentYear = new Date().getFullYear(); // Создаем состояние для текущего года
+	const NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+	const OPERATORS = ['C', '+', '-', '='];
+	const resultContent = '';
 
-	// return (
-	// 	// здесь использован декларативный стиль написания HTML
-	// 	// императивный стиль HTML  это через createElement
-	// 	<div className="App">
-	// 		<header className="App-header">
-	// 			<img src={logo} className="App-logo" alt="logo" />
-	// 			<p>
-	// 				Edit <code>src/App.js</code> and save to reload.
-	// 			</p>
-	// 			<a
-	// 				className="App-link"
-	// 				href="https://reactjs.org"
-	// 				target="_blank"
-	// 				rel="noopener noreferrer"
-	// 			>
-	// 				Learn React
-	// 			</a>
-	// 			<p>Текущий год: {currentYear}</p>
-	// 		</header>
-	// 	</div>
-	// );
+	const Num0 = () => {
+		console.log('Клик 0');
+	};
+	const Num1 = () => {
+		console.log('Клик 1');
+	};
+	const Num2 = () => {
+		console.log('Клик 2');
+	};
+	const Num3 = () => {
+		console.log('Клик 3');
+	};
+	const Num4 = () => {
+		console.log('Клик 4');
+	};
+	const Num5 = () => {
+		console.log('Клик 5');
+	};
+	const Num6 = () => {
+		console.log('Клик 6');
+	};
+	const Num7 = () => {
+		console.log('Клик 7');
+	};
+	const Num8 = () => {
+		console.log('Клик 8');
+	};
+	const Num9 = () => {
+		console.log('Клик 9');
+	};
 
-	const img = createElement('img', { src: logo, className: 'App-logo', alt: 'logo' });
-	const code = createElement('code', { className: '' }, 'src/App.js');
-	const pEditCode = createElement(
-		'p',
-		{ className: '' },
-		'Edit ',
-		code,
-		' and save to reload.',
+	const butPlus = () => {
+		console.log('Клик +');
+	};
+	const butMinus = () => {
+		console.log('Клик -');
+	};
+	const butClear = () => {
+		console.log('Клик C');
+	};
+	const butResult = () => {
+		console.log('Клик =');
+	};
+	console.log(styles);
+
+	return (
+		// здесь использован декларативный стиль написания HTML
+		// императивный стиль HTML  это через createElement
+		<div>
+			<div className={styles.container}>
+				{/* <header className={styles['app-header']}>Calculate</header> */}
+				<h1 className={styles['page-heading']}>Калькулятор</h1>
+
+				{/* кнопки Назад, Вперед и Начать сначала */}
+				<div className={styles['buttons-container']}>
+					<div className={styles['result-content']}>{resultContent}</div>
+					<button id="butClear" className={styles.button} onClick={butClear}>
+						C
+					</button>
+					<button id="butPlus" className={styles.button} onClick={butPlus}>
+						+
+					</button>
+					<button id="butMinus" className={styles.button} onClick={butMinus}>
+						-
+					</button>
+
+					<button id="Num1" className={styles.button} onClick={Num1}>
+						1
+					</button>
+					<button id="Num2" className={styles.button} onClick={Num2}>
+						2
+					</button>
+					<button id="Num3" className={styles.button} onClick={Num3}>
+						3
+					</button>
+					<button id="Num4" className={styles.button} onClick={Num4}>
+						4
+					</button>
+					<button id="Num5" className={styles.button} onClick={Num5}>
+						5
+					</button>
+					<button id="Num6" className={styles.button} onClick={Num6}>
+						6
+					</button>
+					<button id="Num7" className={styles.button} onClick={Num7}>
+						7
+					</button>
+					<button id="Num8" className={styles.button} onClick={Num8}>
+						8
+					</button>
+					<button id="Num9" className={styles.button} onClick={Num9}>
+						9
+					</button>
+					<button
+						id="Num0"
+						className={`${styles.button} ${styles['button-zero']}`}
+						onClick={Num0}
+					>
+						0
+					</button>
+					<button
+						id="butResult"
+						className={`${styles.button} ${styles['button-equal']}`}
+						onClick={butResult}
+					>
+						=
+					</button>
+				</div>
+			</div>
+		</div>
 	);
-	const a = createElement(
-		'a',
-		{
-			className: 'App-link',
-			href: 'https://reactjs.org',
-			target: '_blank',
-			rel: 'noopener noreferrer',
-		},
-		'Learn React',
-	);
-	const pYear = createElement('p', { className: '' }, 'Текущий год: ', currentYear);
-
-	const header = createElement(
-		'header',
-		{ className: 'App-header' },
-		img,
-		pEditCode,
-		a,
-		pYear,
-	);
-	const divApp = createElement('div', { className: 'App' }, header);
-
-	return divApp;
 };
